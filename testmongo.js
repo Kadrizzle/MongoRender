f0dd15920592336680c462f8fcb460e170bf403d
Kadrizzle <lanekade8@gmail.com>
2024-03-03 01:36:15 -0600

    changes

--- testmongo.js
+++ testmongo.js
@@ -41,7 +41,7 @@ app.get("/api/mongo/:item", function (req, res) {
       // Hardwired Query for a part that has partID '12345'
       // const query = { partID: '12345' };
       // But we will use the parameter provided with the route
-      const query = { partID: req.params.item };
+      const query = { age: req.params.item };
 
       const part = await parts.findOne(query);
       console.log(part);
